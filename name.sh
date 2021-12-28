d29574f697c27325f1e5084ddac9aebc95789823
bikkumar <bikash_shit@persistent.com>
2021-12-28 10:42:30 +0530

    var test

--- name.sh
+++ name.sh
@@ -17,18 +17,16 @@ MIME-Version: 1.0
 Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 
---//
-#!/bin/bash
- echo "i am bikash" > /tmp/bikkumar
 
+#!/bin/bash
+ echo \${var.server} > /tmp/bikkumar
  sudo apt update 
  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
  curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
  sudo add-apt-repository "deb https://debian.neo4j.com stable 4.1"
-
  sudo apt install -y neo4j
  sudo systemctl enable neo4j.service
-   
  sudo sed -i 's/#dbms.default_listen_address=0.0.0.0/dbms.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
  sudo systemctl stop neo4j.service
  sudo systemctl start neo4j.service
+--//--
