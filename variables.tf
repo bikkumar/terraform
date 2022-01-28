dd31c20e4a6a274998eea359007bfa7dc829cdb2
bikkumar <bikash_shit@persistent.com>
2022-01-28 09:38:37 +0530

    last latest

--- variables.tf
+++ variables.tf
@@ -6,4 +6,13 @@ variable "env" {
   type = string
   default = "dev"
 }
+variable "public_subnet_cidr_blocks" {
+  description = "Available cidr blocks for public subnets."
+  type        = list(string)
+  default     = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+  ]
+}
 
